ab6b86dcf49abb42655c8cc75821689f26762caa
thomas <thomas@sixfeetup.com>
2010-09-29 20:36:08 +0000

    product for zine import through transmogrifier, refs #558

---
+++ transmogrify/zine/blueprint.py
@@ -0,0 +1,131 @@
+import datetime
+
+from zope.interface import classProvides, implements
+from collective.transmogrifier.interfaces import ISectionBlueprint
+from collective.transmogrifier.interfaces import ISection
+from collective.transmogrifier.utils import Matcher
+from collective.transmogrifier.utils import defaultKeys
+try:
+    from plone.app.discussion.comment import CommentFactory
+    from plone.app.discussion.interfaces import IConversation
+    PAD_INSTALLED = True
+except ImportError:
+    PAD_INSTALLED = False
+from plone.intelligenttext.transforms import\
+        convertHtmlToWebIntelligentPlainText
+
+class PloneFieldsFC(object):
+    """ This section edits the post's fields to be ready for item creation.
+    """
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+
+    def __iter__(self):
+        for item in self.previous:
+            item['_id'] = \
+                item['_transmogrify.zine.id'].split('/')[-1].replace(':','')
+            item['_path'] = '/blog/%s' %(item['_id'])
+            item['_raw_title'] = item['_transmogrify.zine.title']
+            item['creators'] = [item['_transmogrify.zine.author.name']]
+            item['effectiveDate'] = item['_transmogrify.zine.updated.rfc822']
+            item['modificationDate'] =\
+                    item['_transmogrify.zine.updated.rfc822']
+            item['creationDate'] = item['_transmogrify.zine.published.rfc822']
+            item['_transitions'] = item['_transmogrify.zine.state'] ==\
+                    'published' and 'publish' or None
+            item['_comments'] = item['_transmogrify.zine.comments']
+            item['allow_discussion'] = True
+            yield item
+
+
+class Format(object):
+    """ This section edits the post's body text before item creation.
+    """
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.image_base = options['image_base']
+
+    def __iter__(self):
+        for item in self.previous:
+            text = item['_transmogrify.zine.html']
+            images = text.split('<img')[1:]
+            for image in images:
+                image = image.split('>')[0]
+                if 'src="/' in image:
+                    text = text.replace(image,
+                        image.replace('src="/',
+                        'src="%s' %(self.image_base))
+                                       )
+            item['text'] = text
+            yield item
+
+
+class CommentsSection(object):
+    """A blueprint for importing comments into plone
+"""
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.context = transmogrifier.context
+        if 'path-key' in options:
+            pathkeys = options['path-key'].splitlines()
+        else:
+            pathkeys = defaultKeys(options['blueprint'], name, 'path')
+        self.pathkey = Matcher(*pathkeys)
+        self.comment_type = options.get("comment-type", "plone")
+        self.enabled = True
+        if self.comment_type == "plone.app.discussion" and not PAD_INSTALLED:
+            # TODO: log a note
+            self.enabled = False
+
+
+    def __iter__(self):
+        for item in self.previous:
+            pathkey = self.pathkey(*item.keys())[0]
+            # item doesn't exist or the type of comment cannot be
+            # created
+            if not self.enabled or not pathkey:
+                yield item
+                continue
+
+            path = item[pathkey]
+            obj = self.context.unrestrictedTraverse(path.lstrip('/'), None)
+            # path doesn't exist
+            if obj is None:
+                yield item
+                continue
+
+            # TODO: check to see if the object supports commenting...
+            comments = item.get('_comments', [])
+            for comment in comments:
+                title = comment.get('title', '')
+                text = comment.get('text', '')
+                creator = comment.get('author.name', '')
+                creation_date = comment.get('published', '')
+                modification_date = comment.get('updated', '')
+                if self.comment_type == "plone.app.discussion":
+                    conversation = IConversation(obj)
+                    # create a reply object
+                    comment = CommentFactory()
+                    comment.title = title
+                    comment.text = convertHtmlToWebIntelligentPlainText(text)
+                    comment.creator = creator
+                    # TODO: check if the date is a datetime instance
+                    comment.creation_date = creation_date or\
+                            datetime.datetime.now()
+                    comment.modification_date = modification_date or\
+                            datetime.datetime.now()
+                    conversation.addComment(comment)
+                    # TODO: fire events
+                if self.comment_type == "plone":
+                    # TODO: create default plone content
+                    pass
+            yield item
--- transmogrify/zine/source.py
+++ transmogrify/zine/source.py
@@ -8,13 +8,11 @@ from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.utils import resolvePackageReferenceOrFile
 
 SETTINGS_KEY = "transmogrify.zine.settings"
-SETTINGS_SCHEMA = "http://schemas.google.com/blogger/2008/kind#settings"
-POST_SCHEMA = "http://schemas.google.com/blogger/2008/kind#post"
-COMMENT_SCHEMA = "http://schemas.google.com/blogger/2008/kind#comment"
 ATOM_URL = "http://www.w3.org/2005/Atom"
 ATOM_NAMESPACE = "{%s}" % ATOM_URL
 BLOGGER_NAMESPACES = {
     'a': ATOM_URL,
+    'z': 'http://zine.pocoo.org/',
     'thr': 'http://purl.org/syndication/thread/1.0',
     'app': 'http://purl.org/atom/app#',
     }
@@ -42,15 +40,6 @@ class ZineSource(object):
         self.storage = IAnnotations(transmogrifier).setdefault(
             SETTINGS_KEY, {})
         # grab the settings from the xml feed
-        self.init_settings()
-
-    def init_settings(self):
-        settings = self.xml_root.xpath(
-            "a:entry/a:category[contains(@term, '%s')]/.." % SETTINGS_SCHEMA,
-            namespaces=BLOGGER_NAMESPACES)
-        for setting in settings:
-            setting_key = setting.id.text.split(".")[-1]
-            self.storage[setting_key] = setting.content.text
 
     def init_xml_obj(self, filename):
         xml_file = open(filename)
@@ -58,75 +47,66 @@ class ZineSource(object):
         self.xml_root = self.xml_obj.getroot()
         xml_file.close()
 
-    def extract_comments(self, post_id):
+    def extract_comments(self, post):
         parsed_comments = []
-        comments = self.xml_root.xpath(
-            "a:entry/thr:in-reply-to[@ref='%s']/.." % post_id,
+        comments = post.xpath(
+            "z:comment",
             namespaces=BLOGGER_NAMESPACES)
         for comment in comments:
             item = {}
-            item['id'] = comment.id.text
-            item['title'] = comment.title.text
+            item['id'] = comment.xpath("@id")[0]
             item['text'] = comment.content.text
             item['author.name'] = comment.author.name.text
             # the uri may not exist, use find to get it
-            item['author.url'] = comment.author.find("%suri" % ATOM_NAMESPACE)
+            item['author.url'] = comment.author.find(
+                "{%s}uri" % BLOGGER_NAMESPACES['z']
+            )
             item['author.email'] = comment.author.email.text
             published = parse(comment.published.text)
             item['published'] = published
             published_rfc822 = published.strftime(RFC822_FMT)
             item['published.rfc822'] = published_rfc822
-            updated = parse(comment.updated.text)
-            item['updated'] = updated
-            updated_rfc822 = updated.strftime(RFC822_FMT)
-            item['updated.rfc822'] = updated_rfc822
-            alt_link = comment.xpath(
-                "a:link[@rel='alternate']/@href",
-                namespaces=BLOGGER_NAMESPACES)
-            item['link'] = alt_link and alt_link[0] or ""
             parsed_comments.append(item)
         return parsed_comments
 
     def __iter__(self):
         # add any other sources into the stream
+
         for item in self.previous:
             yield item
         # process the blog posts
         posts = self.xml_root.xpath(
-            "a:entry/a:category[contains(@term, '%s')]/.." % POST_SCHEMA,
-            namespaces=BLOGGER_NAMESPACES)
+            "a:entry[contains(@xml:base, 'http://www.sixfeetup.com/blog/')]",
+        namespaces=BLOGGER_NAMESPACES)
         for post in posts:
             item = {}
-            item['_transmogrify.blogger.id'] = post.id.text
-            item['_transmogrify.blogger.title'] = post.title.text
-            item['_transmogrify.blogger.content'] = post.content.text
-            item['_transmogrify.blogger.author.name'] = post.author.name.text
-            item['_transmogrify.blogger.author.uri'] = post.author.uri.text
-            item['_transmogrify.blogger.author.email'] = post.author.email.text
+            item['_transmogrify.zine.id'] = post.id.text
+            item['_transmogrify.zine.title'] = post.title.text
+            item['_transmogrify.zine.content'] = post.content.text
+            item['_transmogrify.zine.html'] =\
+                    post.xpath("child::*[attribute::type='html']")[0].text
+            item['_transmogrify.zine.author.name'] = post.author.name.text
+            item['_transmogrify.zine.author.email'] = post.author.email.text
             published = parse(post.published.text)
-            item['_transmogrify.blogger.published'] = published
+            item['_transmogrify.zine.published'] = published
             published_rfc822 = published.strftime(RFC822_FMT)
-            item['_transmogrify.blogger.published.rfc822'] = published_rfc822
+            item['_transmogrify.zine.published.rfc822'] = published_rfc822
             updated = parse(post.updated.text)
-            item['_transmogrify.blogger.updated'] = updated
+            item['_transmogrify.zine.updated'] = updated
             updated_rfc822 = updated.strftime(RFC822_FMT)
-            item['_transmogrify.blogger.updated.rfc822'] = updated_rfc822
+            item['_transmogrify.zine.updated.rfc822'] = updated_rfc822
             alt_link = post.xpath(
                 "a:link[@rel='alternate']/@href",
                 namespaces=BLOGGER_NAMESPACES)
             alt_link = alt_link and alt_link[0] or ""
-            item['_transmogrify.blogger.link'] = alt_link
+            item['_transmogrify.zine.link'] = alt_link
             post_state = "published"
             draft = post.xpath(
                 "app:control/app:draft",
                 namespaces=BLOGGER_NAMESPACES)
             if draft and draft[0] == "yes":
                 post_state = "draft"
-            item['_transmogrify.blogger.state'] = post_state
-            comment_total = post.xpath(
-                "thr:total",
-                namespaces=BLOGGER_NAMESPACES)[0]
-            item['_transmogrify.blogger.comments.number'] = int(comment_total)
-            comments = self.extract_comments(post.id.text)
-            item['_transmogrify.blogger.comments'] = comments
+            item['_transmogrify.zine.state'] = post_state
+            comments = self.extract_comments(post)
+            item['_transmogrify.zine.comments'] = comments
             yield item
